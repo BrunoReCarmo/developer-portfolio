5466cd437af0ea7114f77eb8c272b4a3554ad38a
BrunoReCarmo <bruno.reis.carmo@gmail.com>
2024-08-21 13:00:18 -0300

    Flags by Lang from URL

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,14 @@
 "use client"
 
-import Link from "next/link"
-import { nav } from "@/constants"
+import { useEffect } from "react";
+import Link from "next/link";
+import { useParams } from "next/navigation";
+
 import { useTheme } from "next-themes"
-import { Logo } from "./logo"
+import "/node_modules/flag-icons/css/flag-icons.min.css";
+
 //Components
-import { Package2, Menu, Sun, Moon } from "lucide-react"
+import { Sun, Moon } from "lucide-react"
 import { Button } from "./ui/button"
 import {
     DropdownMenu,
@@ -41,12 +44,55 @@ function ModeToggle() {
     )
 }
 
+const LangToggle: React.FC = () => {
+    const params = useParams();
+    var lang = params.lang
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button variant="outline" size="icon">
+                    {lang === "pt" ? (
+                        <span className="fi fi-br h-[1.5rem] w-[1.5rem] rotate-0 scale-100 transition-all rounded-lg"></span>
+                    ) : lang === "en" ? (
+                        <span className="fi fi-us h-[1.5rem] w-[1.5rem] rotate-0 scale-100 transition-all rounded-lg"></span>
+                    ) : (
+                        <span className="fi fi-es h-[1.5rem] w-[1.5rem] rotate-0 scale-100 transition-all rounded-lg"></span>
+                    )}
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+                <DropdownMenuItem>
+                    <Link href="/pt" className="flex items-center">
+                    <span className="fi fi-br h-[1.5rem] w-[1.5rem] rotate-0 scale-100 transition-all mr-2 rounded-lg"></span>
+                    Português
+                    </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                <Link href="/en" className="flex items-center">
+                    <span className="fi fi-us h-[1.5rem] w-[1.5rem] rotate-0 scale-100 transition-all mr-2 rounded-lg"></span>
+                    English
+                </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                <Link href="/es" className="flex items-center">
+                    <span className="fi fi-es h-[1.5rem] w-[1.5rem] rotate-0 scale-100 transition-all mr-2 rounded-lg"></span>
+                    Español
+                </Link>
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+    
+}
+
 export const Header = () => {
     return (
         <div className="flex z-10 fixed justify-center backdrop-blur-sm bg-opacity-40 ">
             <div className="flex-col w-screen ">
                 <header className="top-0 flex h-16 items-center gap-4 px-4 md:px-6">
-                    <form className="ml-auto flex-1 flex-initial">
+                    <form className="ml-auto flex space-x-2 items-center">
+                        <LangToggle />
                         <ModeToggle />
                     </form>
                 </header>
